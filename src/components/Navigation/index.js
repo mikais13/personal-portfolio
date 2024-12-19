f078c939809e43492d107b60e3383275af61a5cc
mikais13 <mikai.somerville@gmail.com>
2024-12-19 21:57:47 +1300

    refactor Navigation component layout and styles for improved structure and responsiveness, forming vertical columns

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -8,23 +8,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Navigation() {
     return (
         <header>
-            <div className="start">
-                <h1><code>Mikai Somerville</code></h1>
-            </div>
-            <div className="middle">
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
-                <NavLink to="/experience">Experience</NavLink>
+            <div className="top">
+                <div className="start">
+                    <h1><code>Mikai Somerville</code></h1>
+                </div>
+                <div className="middle">
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/about">About</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
+                    <NavLink to="/experience">Experience</NavLink>
+                </div>
             </div>
             <div className="end">
-                <a className="tiny-link" href="https://linkedin.com/in/mikaisomerville" alt="LinkedIn">
+                <a className="logo-link" href="https://linkedin.com/in/mikaisomerville" alt="LinkedIn">
                     <FontAwesomeIcon icon={faLinkedin} />
                 </a>
-                <a className="tiny-link" href="https://github.com/mikais13" alt="GitHub">
+                <a className="logo-link" href="https://github.com/mikais13" alt="GitHub">
                     <FontAwesomeIcon icon={faSquareGithub} />
                 </a>
-                <a className="tiny-link" href="" alt="Resume">
+                <a className="logo-link" href="" alt="Resume">
                     <FontAwesomeIcon icon={faFile} />
                 </a>
             </div>
