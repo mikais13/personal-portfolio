2519cc67425929dea6732d3afb746db9d2ca7260
mikais13 <mikai.somerville@gmail.com>
2024-12-22 22:00:55 +1300

    add Home component styles and added example content;

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,18 @@
 import React from "react";
+import "./index.scss";
 
 export default function Home() {
     return (
         <div className="page-container">
-            <h1>Home</h1>
-            <p>This is the home page.</p>
+            <div className="title-container">
+                <h1>Home</h1>
+            </div>
+            <div className="content-container">
+                <p>
+                    Welcome to my portfolio! I'm a passionate developer focused on creating innovative solutions.
+                    I specialize in full-stack development with expertise in React, Node.js, and modern web technologies. With a strong foundation in software engineering principles, I strive to build scalable and maintainable applications. Throughout my career, I've worked on diverse projects ranging from e-commerce platforms to real-time data visualization systems. I'm always eager to learn new technologies and tackle challenging problems.
+                </p>
+            </div>
         </div>
     );
 }
