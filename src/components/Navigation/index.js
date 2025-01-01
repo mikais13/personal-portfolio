9390471695ddfb79b6290394e44cbef2adf36a26
mikais13 <mikai.somerville@gmail.com>
2025-01-02 11:26:55 +1300

    Change Navigation styling with active and non-active classes with hover effects

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,31 +1,47 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import "./index.scss";
 import { faSquareGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faFile } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Navigation() {
+    const { pathname } = useLocation();
     return (
         <header>
             <div className="top">
-                <div className="start">
+                <div className={"start"}>
                     <h1><code>Mikai Somerville</code></h1>
                 </div>
                 <div className="middle">
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="/about">About Me</NavLink>
-                    <NavLink to="/experience">Experience</NavLink>
+                    <div className={`nav-link ${pathname === "/" ? "active" : ""}`}>
+                        <NavLink to="/">
+                            <div className="line"></div>
+                            <p>Home</p>
+                        </NavLink>
+                    </div>
+                    <div className={`nav-link ${pathname === "/about" ? "active" : ""}`}>
+                        <NavLink to="/about">
+                            <div className="line"></div>
+                            <p>About Me</p>
+                        </NavLink>
+                    </div>
+                    <div className={`nav-link ${pathname === "/experience" ? "active" : ""}`}>
+                        <NavLink to="/experience">
+                            <div className="line"></div>
+                            <p>Experience</p>
+                        </NavLink>
+                    </div>
                 </div>
             </div>
             <div className="end">
-                <a className="logo-link" href="https://linkedin.com/in/mikaisomerville" alt="LinkedIn">
+                <a className="nav-link logo-link" href="https://linkedin.com/in/mikaisomerville" alt="LinkedIn">
                     <FontAwesomeIcon icon={faLinkedin} />
                 </a>
-                <a className="logo-link" href="https://github.com/mikais13" alt="GitHub">
+                <a className="nav-link logo-link" href="https://github.com/mikais13" alt="GitHub">
                     <FontAwesomeIcon icon={faSquareGithub} />
                 </a>
-                <a className="logo-link" href="" alt="Resume">
+                <a className="nav-link logo-link" href="" alt="Resume">
                     <FontAwesomeIcon icon={faFile} />
                 </a>
             </div>
