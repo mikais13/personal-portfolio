3e53f9a9dd2e3c7945d4282b16fec6455e76aeea
mikais13 <mikai.somerville@gmail.com>
2025-01-06 20:05:54 +1300

    Refactor date display logic in ExperienceCard component

--- src/components/Experience/ExperienceCard.js
+++ src/components/Experience/ExperienceCard.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 export default function ExperienceCard({ experience }) {
+    var date = "";
+    if (experience.startDate != null) {
+        date += experience.startDate;
+        if (experience.endDate != null) {
+            date += " - " + experience.endDate;
+        }
+    }
+
     return (
         <div className='experience-card'>
             <div className="header">
                 <p className='title'>{experience.title}</p>
-                {
-                    experience.startDate != null || experience.endDate != null ?
-                    <div className='date'>
-                        <p>{ `${experience.startDate} - ${experience.endDate}`}</p>
-                    </div> : null
-                }
+                <div className='date'>
+                    <p>{date}</p>
+                </div>
             </div>
             <div className='info'>
                 <p className='company'>{experience.company}</p>
