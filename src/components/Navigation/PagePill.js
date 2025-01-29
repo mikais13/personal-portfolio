3b398701c5a483bf2054d8219f351933b44667fb
mikais13 <mikai.somerville@gmail.com>
2025-01-29 20:09:36 +1300

    Enhance PagePill component with animations and responsive layout adjustments; add AnimatePresence for smoother transitions

--- src/components/Navigation/PagePill.js
+++ src/components/Navigation/PagePill.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
@@ -8,26 +8,28 @@ export default function PagePill({ left, middle, right }) {
     const [active, setActive] = useState("");
 
     return (
-        <div className="page-link">
-            <div className="line">
-                <NavLink to={`/${left === "home" ? "" : left}`} className={`left ${active !== "" ? "active" : ""}`} onMouseEnter={() => setActive("left")} onMouseLeave={() => setActive("")}>
-                    <p className="inner" layout>
-                        <FontAwesomeIcon icon={faArrowLeft} />
-                        {left[0].toUpperCase() + left.slice(1)}
-                    </p>
-                </NavLink>
-                <NavLink to={`/${middle === "home" ? "" : middle}`} className={`middle ${active !== "" ? "active" : ""}`} onMouseEnter={() => setActive("middle")} onMouseLeave={() => setActive("")}>
-                    <p className="inner" layout>
-                        Next Page
-                    </p>
-                </NavLink>
-                <NavLink to={`/${right === "home" ? "" : middle}`} className={`right ${active !== "" ? "active" : ""}`} onMouseEnter={() => setActive("right")} onMouseLeave={() => setActive("")}>
-                    <p className="inner" layout>
-                        {right[0].toUpperCase() + right.slice(1)}
-                        <FontAwesomeIcon icon={faArrowRight} />
-                    </p>
-                </NavLink>
-            </div>
-        </div>
+        <AnimatePresence>
+            <motion.div className="page-link" layoutId="page-pill" transition={{ duration: 0.33 }}>
+                <div className="line">
+                    <NavLink to={`/${left === "home" ? "" : left}`} className={`left ${active !== "" ? "active" : ""}`} onMouseEnter={() => setActive("left")} onMouseLeave={() => setActive("")}>
+                        <p className="inner" layout>
+                            <FontAwesomeIcon icon={faArrowLeft} />
+                            {left[0].toUpperCase() + left.slice(1)}
+                        </p>
+                    </NavLink>
+                    <NavLink to={`/${middle === "home" ? "" : middle}`} className={`middle ${active !== "" ? "active" : ""}`} onMouseEnter={() => setActive("middle")} onMouseLeave={() => setActive("")}>
+                        <p className="inner" layout>
+                            Next Page
+                        </p>
+                    </NavLink>
+                    <NavLink to={`/${right === "home" ? "" : middle}`} className={`right ${active !== "" ? "active" : ""}`} onMouseEnter={() => setActive("right")} onMouseLeave={() => setActive("")}>
+                        <p className="inner" layout>
+                            {right[0].toUpperCase() + right.slice(1)}
+                            <FontAwesomeIcon icon={faArrowRight} />
+                        </p>
+                    </NavLink>
+                </div>
+            </motion.div>
+        </AnimatePresence>
     );
 }
