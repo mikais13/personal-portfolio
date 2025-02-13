9f43a386355036a722426f62a223e09a8fe98868
mikais13 <mikai.somerville@gmail.com>
2025-02-13 20:54:07 +1300

    Replace index.scss with index.css for consistent styling; update font imports accordingly

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@fontsource-variable/inter';
-import './index.scss';
+import './index.css';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 
