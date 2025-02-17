d023c2ccb47aecaa94c6f0463702742566926559
mikais13 <mikai.somerville@gmail.com>
2025-02-17 17:03:58 +1300

    Remove loaders.css dependency and associated loader styles for cleaner codebase

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import Navigation from "../Navigation";
 import { Outlet } from "react-router-dom";
 import "./index.scss";
-import "../../utils/loader.scss";
 
 export default function Root() {
     return (
