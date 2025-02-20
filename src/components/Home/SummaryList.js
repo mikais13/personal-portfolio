4cc6c131f71d3b4bc07690f40fa4f6a39e616202
mikais13 <mikai.somerville@gmail.com>
2025-02-20 17:52:30 +1300

    Refactor SummaryList component by removing unused code and enhancing layout with improved scrolling behavior and styling adjustments

--- src/components/Home/SummaryList.js
+++ src/components/Home/SummaryList.js
@@ -1,40 +1,10 @@
-import React, { useState, useRef } from 'react';
+import React, { useRef } from 'react';
 import { motion } from 'motion/react';
 
 export default function SummaryList({ id, summaries }) {
-  const [hovering, setHovering] = useState(false);
-  const carouselRef = useRef(null);
-  // const [baseVelocity, setBaseVelocity] = useState(-10);
-  // const baseX = useMotionValue(0);
-  // const { scrollX } = useScroll({
-  //   container: carouselRef,
-  //   axis: 'x',
-  // });
-  // const scrollVelocity = useVelocity(scrollX);
-  // const smoothVelocity = useSpring(scrollVelocity, {
-  //   damping: 50,
-  //   stiffness: 400
-  // });
-  // const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
-  //   clamp: false
-  // });
-  // const x = useTransform(baseX, (v) => `${wrap(0, -100, v)}%`);
-  // // const x = useTransform(baseX, (value) => {
-  // //   if (baseX.get() >= 1 || baseX.get() <= -101) {
-  // //     setBaseVelocity(-1 * baseVelocity);
-  // //   }
-  // //   return `${value}%`;
-  // // })
-  // useAnimationFrame((time, delta) => {
-  //   if (hovering) {
-  //     baseX.set(baseX.get() + scrollVelocity.get());
-  //   } else {
-  //     let moveBy = baseVelocity * (delta / 1000);
-  //     moveBy += moveBy * velocityFactor.get();
-  //     baseX.set(baseX.get() + moveBy);
-  //   }
-  // });
-  const innerWidth = carouselRef.current ? carouselRef.current.scrollWidth : 0;
+  const firstSet = useRef(null);
+  const secondSet = useRef(null);
+  const innerWidth = firstSet.current ? firstSet.current.scrollWidth : 0;
 
   return (
     <div
@@ -42,13 +12,8 @@ export default function SummaryList({ id, summaries }) {
     >
       <motion.div
         id={id}
-        ref={carouselRef}
+        ref={firstSet}
         className='summary-list'
-        // initial={{ opacity: 0, y: 16 }}
-        // animate={{ opacity: 1, y: 0 }}
-        // style={{ x }}
-        onHoverStart={() => setHovering(true)}
-        onHoverEnd={() => setHovering(false)}
         initial={{ x: 0 }}
         animate={{ x: -innerWidth }}
         transition={{ duration: 10, ease: 'linear', repeat: Infinity }}
@@ -61,13 +26,8 @@ export default function SummaryList({ id, summaries }) {
       </motion.div>
       <motion.div
         id={id}
-        ref={carouselRef}
+        ref={secondSet}
         className='summary-list'
-        // initial={{ opacity: 0, y: 16 }}
-        // animate={{ opacity: 1, y: 0 }}
-        // style={{ x }}
-        onHoverStart={() => setHovering(true)}
-        onHoverEnd={() => setHovering(false)}
         initial={{ x: 0 }}
         animate={{ x: -innerWidth }}
         transition={{ duration: 10, ease: 'linear', repeat: Infinity }}
@@ -86,8 +46,9 @@ export function SummaryCard({ experience }) {
   return (
     <motion.div
       className='summary-card'
-      initial={{ x: 32 }}
+      initial={{ x: 32, y: 16, scale: 0.8 }}
       animate={{ x: 0 }}
+      whileInView={{ x: 0, y: 0, scale: 1 }}
     >
       <div className='title'>
         <h2>{experience.title}</h2>
